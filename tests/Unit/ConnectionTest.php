e46220868c60784f33b5200054a6527a4c799413
Nizigama jean davy <jedavy.n@gmail.com>
2022-03-09 20:20:42 +0200

    Created a test folder

---
+++ tests/Unit/ConnectionTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+
+class ConnectionTest extends TestCase{
+
+    /** @test */
+    public function can_create_a_connection(){
+        $this->assertTrue(true);
+    }
+}
